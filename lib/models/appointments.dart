8a7ac8837190d17beafc7b5b1d0e0c914d2e01d1
Parker Roth <parkeroth@gmail.com>
2021-06-21 16:18:13 -0500

    Appointment info validation.

--- lib/models/appointments.dart
+++ lib/models/appointments.dart
@@ -17,7 +17,7 @@ class Appointments extends ChangeNotifier {
     return appointments;
   }
 
-  String? add(int clientId, DateTime startTime, Duration duration) {
+  String? add(int clientId, DateTime startTime, Duration duration, AppointmentType type) {
     var appointments = _appointments[clientId];
     if (appointments != null) {
       var overlappingAppointment;
@@ -31,7 +31,7 @@ class Appointments extends ChangeNotifier {
         return "Found overlapping appointment $overlappingAppointment";
       }
     }
-    var appointment = Appointment(startTime, duration, clientId);
+    var appointment = Appointment(type, startTime, duration, clientId);
     if (appointments == null) {
       _appointments[clientId] = [appointment];
     } else {
@@ -66,11 +66,69 @@ class Appointments extends ChangeNotifier {
   }
 }
 
+enum AppointmentType {
+  GENERIC,
+  INFO,
+  INTRO,
+  FUP1,
+  FUP2,
+  FUP3,
+  FUP4,
+  FUP5,
+  FUP6,
+  FUP7,
+  FUP8,
+  FUP9,
+  FUP10,
+  FUP11,
+  FUP12,
+  PREG_EVAL
+}
+
+extension AppointmentTypeExt on AppointmentType {
+  AppointmentType? next() {
+    switch(this) {
+      case AppointmentType.INTRO:
+        return AppointmentType.FUP1;
+      case AppointmentType.FUP1:
+        return AppointmentType.FUP2;
+      case AppointmentType.FUP2:
+        return AppointmentType.FUP3;
+      case AppointmentType.FUP3:
+        return AppointmentType.FUP4;
+      case AppointmentType.FUP4:
+        return AppointmentType.FUP5;
+      case AppointmentType.FUP5:
+        return AppointmentType.FUP6;
+      case AppointmentType.FUP6:
+        return AppointmentType.FUP7;
+      case AppointmentType.FUP7:
+        return AppointmentType.FUP8;
+      case AppointmentType.FUP8:
+        return AppointmentType.FUP9;
+      case AppointmentType.FUP9:
+        return AppointmentType.FUP10;
+      case AppointmentType.FUP10:
+        return AppointmentType.FUP11;
+      case AppointmentType.FUP11:
+        return AppointmentType.FUP12;
+      default:
+        return null;
+    }
+  }
+
+  String name() {
+    return toString().split(".")[1];
+  }
+}
+
 class Appointment {
+  AppointmentType type;
   DateTime time;
   Duration duration;
   int clientId;
   Appointment(
+      this.type,
       this.time,
       this.duration,
       this.clientId,
--- lib/screens/appointment_info.dart
+++ lib/screens/appointment_info.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:lmlb/models/appointments.dart';
+import 'package:lmlb/models/clients.dart';
+import 'package:provider/provider.dart';
 
 class AppointmentInfoScreenArguments {
   final Appointment? appointment;
@@ -30,11 +32,54 @@ class AppointmentInfoForm extends StatefulWidget {
 }
 
 class AppointmentInfoFormState extends State<AppointmentInfoForm> {
-  final Appointment? _appointment;
-
-  AppointmentInfoFormState(this._appointment);
-
   final _formKey = GlobalKey<FormState>();
+
+  late final Appointment? _appointment;
+  int? _clientId;
+  DateTime? _appointmentDate;
+  TimeOfDay? _appointmentTime;
+  AppointmentType? _appointmentType;
+
+  AppointmentInfoFormState(Appointment? appointment) {
+    this._appointment = appointment;
+    this._clientId = _appointment?.clientId;
+    this._appointmentType = _appointment?.type;
+    this._appointmentDate = _appointment?.time;
+    this._appointmentTime = _appointmentDate == null
+        ? null
+        : TimeOfDay.fromDateTime(_appointmentDate!);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance!.addPostFrameCallback((_) {
+      if (Provider.of<Clients>(context, listen: false).clients.isEmpty) {
+        Widget continueButton = TextButton(
+          child: Text("Ack"),
+          onPressed: () {
+            Navigator.of(context).popUntil((route) => route.isFirst);
+          },
+        );
+        // set up the AlertDialog
+        AlertDialog alert = AlertDialog(
+          title: Text("No Clients Found"),
+          content:
+              Text("Please add the client before scheduling an appointment"),
+          actions: [
+            continueButton,
+          ],
+        );
+        // show the dialog
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return alert;
+          },
+        );
+      }
+    });
+  }
 
   @override
   void dispose() {
@@ -46,7 +91,8 @@ class AppointmentInfoFormState extends State<AppointmentInfoForm> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(_appointment == null ? 'New Client' : 'Client Info'),
+        title:
+            Text(_appointment == null ? 'New Appointment' : 'Appointment Info'),
         actions: [
           TextButton.icon(
             style: TextButton.styleFrom(primary: Colors.white),
@@ -62,7 +108,12 @@ class AppointmentInfoFormState extends State<AppointmentInfoForm> {
           padding: EdgeInsets.all(10.0),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
-            children: [],
+            children: [
+              _buildClientSelector(context),
+              _buildTypeSelector(context),
+              _buildEventDate(context),
+              _buildEventTime(context),
+            ],
           ),
         ),
       ),
@@ -71,8 +122,169 @@ class AppointmentInfoFormState extends State<AppointmentInfoForm> {
 
   void _onSave() {
     if (_formKey.currentState!.validate()) {
+      Provider.of<Appointments>(context, listen: false).add(
+          _clientId!,
+          DateTime(
+              _appointmentDate!.year,
+              _appointmentDate!.month,
+              _appointmentDate!.day,
+              _appointmentTime!.hour,
+              _appointmentTime!.minute),
+          Duration(hours: 1),
+          _appointmentType!);
       Navigator.of(context).pop(true);
+    } else {
+      print("Validation error");
     }
   }
 
+  Widget _buildEventDate(BuildContext context) {
+    final Widget display = _appointmentDate == null
+        ? Text("Select a date")
+        : Text(_appointmentDate!.toIso8601String());
+    return _buildContainer(
+        "Appointment Date:",
+        FormField(
+            autovalidateMode: AutovalidateMode.onUserInteraction,
+            validator: (value) {
+              if (value == null) {
+                return "Date required";
+              }
+            },
+            builder: (state) => GestureDetector(
+                onTap: () async {
+                  final DateTime? picked = await showDatePicker(
+                    context: context,
+                    initialDate: DateTime.now(), // Refer step 1
+                    firstDate: DateTime.now(),
+                    lastDate: DateTime.now().add(Duration(days: 365)),
+                  );
+                  if (picked != null) {
+                    state.didChange(picked);
+                    setState(() {
+                      _appointmentDate = picked;
+                    });
+                  }
+                },
+                child: _showErrorOrDisplay(state, display))));
+  }
+
+  Widget _buildEventTime(BuildContext context) {
+    final Widget display = _appointmentTime == null
+        ? Text("Select a time")
+        : Text(_appointmentTime!.format(context));
+    return _buildContainer(
+        "Appointment Time:",
+        FormField(
+            autovalidateMode: AutovalidateMode.onUserInteraction,
+            validator: (value) {
+              if (value == null) {
+                return "Value required";
+              }
+            },
+            builder: (state) => GestureDetector(
+                onTap: () async {
+                  final TimeOfDay? picked = await showTimePicker(
+                    context: context,
+                    initialTime: TimeOfDay.now(),
+                  );
+                  if (picked != null) {
+                    state.didChange(picked);
+                    setState(() {
+                      _appointmentTime = picked;
+                    });
+                  }
+                },
+                child: _showErrorOrDisplay(state, display))));
+  }
+
+  Widget _buildTypeSelector(BuildContext context) {
+    return _buildContainer(
+        "Appointment Type:",
+        FormField(
+            autovalidateMode: AutovalidateMode.onUserInteraction,
+            validator: (value) {
+              if (value == null) {
+                return "Select a value";
+              }
+            },
+            builder: (state) => _showErrorOrDisplay(
+                state,
+                DropdownButton<AppointmentType>(
+                  hint: Text('Please make a selection'),
+                  items: AppointmentType.values.map((value) {
+                    return DropdownMenuItem<AppointmentType>(
+                      value: value,
+                      child: new Text(value.name()),
+                    );
+                  }).toList(),
+                  onChanged: (selection) {
+                    state.didChange(selection);
+                    setState(() {
+                      _appointmentType = selection;
+                    });
+                  },
+                  value: _appointmentType,
+                ))));
+  }
+
+  Widget _buildClientSelector(BuildContext context) {
+    return _buildContainer(
+        "Client:",
+        FormField(
+            autovalidateMode: AutovalidateMode.onUserInteraction,
+            validator: (value) {
+              if (value == null) {
+                return "Select a value";
+              }
+            },
+            builder: (state) => Consumer<Clients>(
+                builder: (context, clientModel, child) => _showErrorOrDisplay(
+                    state,
+                    DropdownButton<Client>(
+                      hint: Text('Please make a selection'),
+                      items: clientModel.clients.map((client) {
+                        return DropdownMenuItem<Client>(
+                          value: client,
+                          child: new Text(client.fullName()),
+                        );
+                      }).toList(),
+                      onChanged: (selection) {
+                        state.didChange(selection);
+                        setState(() {
+                          _clientId = selection!.num;
+                        });
+                      },
+                      value: _clientId == null
+                          ? null
+                          : clientModel.get(_clientId!),
+                    )))));
+  }
+
+  Widget _showErrorOrDisplay(FormFieldState state, Widget display) {
+    return Column(
+      children: [
+        display,
+        state.hasError
+            ? Text(
+                state.errorText!,
+                style: TextStyle(color: Colors.red),
+              )
+            : Container()
+      ],
+    );
+  }
+
+  Widget _buildContainer(String title, Widget content) {
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 10.0),
+      child: Row(children: [
+        Container(
+          child: Text(title, style: TextStyle(fontWeight: FontWeight.bold)),
+          margin: EdgeInsets.only(right: 10.0),
+        ),
+        content,
+      ]),
+    );
+  }
 }
