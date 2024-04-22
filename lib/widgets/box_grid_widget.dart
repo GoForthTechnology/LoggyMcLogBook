7173d7540175b3b9e20afa958a6848645f8acc1b
Parker Roth <parkeroth@gmail.com>
2024-04-22 14:20:53 -0500

    Improved follow up form input stepper.

---
+++ lib/widgets/box_grid_widget.dart
@@ -0,0 +1,207 @@
+import 'package:fc_forms/src/follow_up_form/models/form_item.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'dart:ui' as ui;
+
+class BoxGridWidget extends StatelessWidget {
+  final List<GridRow> rows;
+  final Row? headerRow;
+  final int nColumns;
+  final bool includeRowLabels;
+
+  static Row? basicHeaderRow(int nColumns) {
+    List<Widget> columnHeadings = [];
+    for (int i = 0; i < nColumns; i++) {
+      columnHeadings.add(LegendCell(
+        text: (i + 1).toString(),
+        width: 30,
+      ));
+    }
+    return Row(children: columnHeadings);
+  }
+
+  static Row? explanationHeaderRow() {
+    return const Row(children: [LegendCell(text: "", width: 30)]);
+  }
+
+  const BoxGridWidget({
+    super.key,
+    required this.headerRow,
+    required this.rows,
+    this.nColumns = 8,
+    this.includeRowLabels = true,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> gridRows = [];
+
+    if (headerRow != null) {
+      gridRows.add(headerRow!);
+    }
+    gridRows.addAll(rows.map((r) => Row(children: r.cells)));
+    Widget grid = Stack(clipBehavior: Clip.none, children: [
+      Positioned(
+        left: 5,
+        top: 5 + (headerRow != null ? 30 : 0),
+        child: Container(
+          width: 30.0 * nColumns,
+          height: 30.0 * rows.length,
+          color: Colors.black,
+        ),
+      ),
+      Column(crossAxisAlignment: CrossAxisAlignment.end, children: gridRows),
+    ]);
+
+    List<Widget> labels = [];
+    if (includeRowLabels) {
+      if (headerRow != null) {
+        labels.add(const LegendCell(
+          text: "",
+          width: 40,
+        ));
+      }
+      for (int i = 0; i < rows.length; i++) {
+        labels.add(LegendCell(
+          text: rows[i].rowLabel,
+          width: 40,
+        ));
+      }
+    }
+    return Column(
+      children: [
+        const Spacer(),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Column(children: labels),
+            grid,
+          ],
+        ),
+        const Spacer(),
+      ],
+    );
+  }
+}
+
+class GridRow {
+  final String rowLabel;
+  final List<Widget> cells;
+
+  GridRow(FollowUpFormItem item, int itemIndex,
+      void Function(BuildContext, int, int) showDialogFn,
+      {int numColumns = 8})
+      : cells = List.generate(
+            numColumns,
+            (index) => BoxWidget(showDialogFn, item, itemIndex, index,
+                disabled: item.disabledCells.contains(index))),
+        rowLabel = createRowLabel(item);
+
+  static String createRowLabel(FollowUpFormItem item) {
+    if (item.subSection == "") {
+      return "";
+    }
+    return "${item.section}${item.superSection ?? ""}${item.subSection}${item.subSubSection ?? ""}";
+  }
+}
+
+class LegendCell extends StatelessWidget {
+  final String text;
+  final double width;
+
+  const LegendCell({super.key, required this.text, required this.width});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: width,
+      height: 30,
+      child: Align(alignment: Alignment.center, child: Text(text)),
+    );
+  }
+}
+
+class BoxWidget extends StatelessWidget {
+  final void Function(BuildContext, int, int) showDialogFn;
+  final FollowUpFormItem item;
+  final int itemIndex;
+  final int followUpIndex;
+  final bool disabled;
+
+  const BoxWidget(
+      this.showDialogFn, this.item, this.itemIndex, this.followUpIndex,
+      {super.key, this.disabled = false});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+        onTap: disabled
+            ? null
+            : () {
+                showDialogFn(context, itemIndex, followUpIndex);
+              },
+        child: Container(
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.black),
+            color: disabled ? Colors.grey[300] : Colors.white,
+          ),
+          width: 30,
+          height: 30,
+          child: item.questions.length > 1
+              ? CustomPaint(
+                  painter: CellPainter(
+                    leftValue: "",
+                    rightValue: "",
+                  ),
+                )
+              : Center(child: Text("")),
+        ));
+  }
+}
+
+class CellPainter extends CustomPainter {
+  final String leftValue;
+  final String rightValue;
+
+  CellPainter({required this.leftValue, required this.rightValue});
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    var paint = Paint()..color = Colors.black;
+    if (leftValue.isNotEmpty) {
+      TextPainter textPainter = TextPainter(
+        text: TextSpan(
+          text: leftValue,
+          style: GoogleFonts.sourceSans3(),
+        ),
+        textDirection: ui.TextDirection.ltr,
+      );
+      textPainter.layout(
+        minWidth: 0,
+        maxWidth: size.width,
+      );
+      textPainter.paint(canvas, const Offset(5, 0));
+    }
+    if (rightValue.isNotEmpty) {
+      TextPainter textPainter = TextPainter(
+        text: TextSpan(
+          text: rightValue,
+          style: GoogleFonts.sourceSans3(),
+        ),
+        textDirection: ui.TextDirection.ltr,
+      );
+      textPainter.layout(
+        minWidth: 0,
+        maxWidth: size.width,
+      );
+      textPainter.paint(canvas, const Offset(15, 10));
+    }
+    canvas.drawLine(Offset(0, size.height), Offset(size.width, 0), paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+}
--- lib/widgets/fup_form.dart
+++ lib/widgets/fup_form.dart
@@ -28,18 +28,20 @@ class FollowUpFormStepperState extends State<FollowUpFormStepper> {
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
         create: (context) => FollowUpFormViewModel(),
-        child: Stepper(
-          currentStep: _index,
-          onStepCancel: _cancelStep,
-          onStepContinue: _continueStep,
-          onStepTapped: _selectStep,
-          steps: itemIndex.entries.map((e) => _step(e.key, e.value)).toList(),
-        ));
+        builder: (context, child) => Stepper(
+              currentStep: _index,
+              onStepCancel: _cancelStep,
+              onStepContinue: _continueStep,
+              onStepTapped: _selectStep,
+              steps:
+                  itemIndex.entries.map((e) => _step(e.key, e.value)).toList(),
+            ));
   }
 
   Step _step(int sectionNum, List<FollowUpFormItem> items) {
     return Step(
       title: Text(sectionTitles[sectionNum] ?? "Section $sectionNum"),
+      //content: FollowUpFormSectionWidget.createSingle([items], groupIndex: 0),
       content: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: items.map(_item).toList(),
@@ -51,79 +53,10 @@ class FollowUpFormStepperState extends State<FollowUpFormStepper> {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: item.questions
-          .mapIndexed(
-              (i, q) => _question(item.entryId(i, widget.followUpNum), item, q))
-          .expand((element) => element)
+          .mapIndexed((i, q) => QuestionWidget(
+              id: item.entryId(i, widget.followUpNum), item: item, question: q))
           .toList(),
     );
-  }
-
-  List<Widget> _question(
-      FollowUpFormEntryId id, FollowUpFormItem item, Question question) {
-    List<Widget> inputWidgets = [
-      ..._inputWidgets(id, question),
-      IconButton(
-          onPressed: () => _addComment(id), icon: Icon(Icons.add_comment))
-    ];
-
-    List<Widget> out = [
-      Padding(
-          padding: EdgeInsets.symmetric(vertical: 2),
-          child: Row(
-            children: [
-              Text("${id.code} - ${question.description}"),
-              Spacer(),
-              ...inputWidgets,
-            ],
-          ))
-    ];
-
-    var comment = comments[id];
-    if (comment != null) {
-      out.add(TextFormField(
-        decoration: InputDecoration(
-          labelText: "Comment",
-          suffixIcon: IconButton(
-            icon: Icon(Icons.clear),
-            onPressed: () => _removeComment(id),
-          ),
-        ),
-        controller: comment,
-      ));
-    }
-    return out;
-  }
-
-  List<Widget> _inputWidgets(FollowUpFormEntryId id, Question question) {
-    inputs.putIfAbsent(id, () => TextEditingController());
-    if (question.acceptableInputs.isNotEmpty) {
-      return question.acceptableInputs
-          .map((input) => ElevatedButton(
-              onPressed: () {
-                if (input == "1") {
-                  _addComment(id);
-                }
-              },
-              child: Text(input)))
-          .toList();
-    }
-    return [
-      ElevatedButton(onPressed: () {}, child: Text("-")),
-      Padding(padding: EdgeInsets.symmetric(horizontal: 6), child: Text("0")),
-      ElevatedButton(onPressed: () {}, child: Text("+")),
-    ];
-  }
-
-  void _addComment(FollowUpFormEntryId id) {
-    setState(() {
-      comments[id] = TextEditingController();
-    });
-  }
-
-  void _removeComment(FollowUpFormEntryId id) {
-    setState(() {
-      comments.remove(id);
-    });
   }
 
   void _cancelStep() {
@@ -147,5 +80,186 @@ class FollowUpFormStepperState extends State<FollowUpFormStepper> {
     setState(() {
       _index = index;
     });
+  }
+}
+
+class QuestionWidget extends StatefulWidget {
+  final FollowUpFormEntryId id;
+  final FollowUpFormItem item;
+  final Question question;
+
+  const QuestionWidget(
+      {super.key,
+      required this.id,
+      required this.item,
+      required this.question});
+
+  @override
+  State<StatefulWidget> createState() => QuestionWidgetState();
+}
+
+class QuestionWidgetState extends State<QuestionWidget> {
+  final TextEditingController inputController = TextEditingController();
+  final Map<int, TextEditingController> commentControllers = {};
+  bool expanded = false;
+
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> out = [
+      Padding(
+          padding: EdgeInsets.symmetric(vertical: 2),
+          child: Row(
+            children: [
+              Text("${widget.id.code} - ${widget.question.description}"),
+              SizedBox(
+                width: 20,
+              ),
+              Spacer(),
+              _inputWidgets(),
+              IconButton(
+                  onPressed: () => _addComment(),
+                  icon: Icon(Icons.add_comment)),
+              ...List.generate(
+                  8,
+                  (index) => Container(
+                        width: 30,
+                        height: 30,
+                        decoration: BoxDecoration(border: Border.all()),
+                      )),
+              IconButton(
+                  onPressed:
+                      commentControllers.isEmpty ? null : _toggleExpansion,
+                  icon: Icon(expanded ? Icons.expand_less : Icons.expand_more)),
+            ],
+          ))
+    ];
+    if (expanded) {
+      var comment = commentControllers[widget.id.followUpIndex];
+      if (comment != null) {
+        out.add(CommentWidget(
+            onRemove: _removeComment,
+            followUpNum: widget.id.followUpIndex + 1));
+      }
+    }
+    return Column(
+      children: out,
+    );
+  }
+
+  void _toggleExpansion() {
+    setState(() {
+      expanded = !expanded;
+    });
+  }
+
+  Widget _inputWidgets() {
+    if (widget.question.acceptableInputs.isNotEmpty) {
+      return ButtonRow(
+        values: widget.question.acceptableInputs,
+        onPressed: (value) {
+          if (value == "1") {
+            _addComment();
+          }
+        },
+      );
+    }
+    return Row(children: [
+      ElevatedButton(onPressed: () {}, child: Text("-")),
+      Padding(padding: EdgeInsets.symmetric(horizontal: 6), child: Text("0")),
+      ElevatedButton(onPressed: () {}, child: Text("+")),
+    ]);
+  }
+
+  void _addComment() {
+    setState(() {
+      commentControllers[widget.id.followUpIndex] = TextEditingController();
+      expanded = true;
+    });
+  }
+
+  void _removeComment() {
+    setState(() {
+      commentControllers.remove(widget.id.followUpIndex);
+      if (commentControllers.isEmpty) {
+        expanded = false;
+      }
+    });
+  }
+}
+
+class CommentWidget extends StatelessWidget {
+  final Function() onRemove;
+  final int followUpNum;
+
+  final commentController = TextEditingController();
+  final planController = TextEditingController();
+
+  CommentWidget({super.key, required this.onRemove, required this.followUpNum});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 20),
+          child: Text("#$followUpNum"),
+        ),
+        Expanded(
+            child: TextFormField(
+          controller: commentController,
+          decoration: InputDecoration(
+            label: Text("Comment"),
+          ),
+          maxLines: null,
+        )),
+        Expanded(
+            child: TextFormField(
+          controller: planController,
+          decoration: InputDecoration(
+            label: Text("Plan of Action"),
+          ),
+          maxLines: null,
+        )),
+        IconButton(onPressed: onRemove, icon: Icon(Icons.clear)),
+      ],
+    );
+  }
+}
+
+class ButtonRow extends StatefulWidget {
+  final Function(String?) onPressed;
+  final List<String> values;
+
+  const ButtonRow({super.key, required this.values, required this.onPressed});
+
+  @override
+  State<ButtonRow> createState() => _ButtonRowState();
+}
+
+class _ButtonRowState extends State<ButtonRow> {
+  String? currentValue = null;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: widget.values.map(_button).toList(),
+    );
+  }
+
+  Widget _button(String value) {
+    var onPressed = () {
+      setState(() {
+        if (currentValue == value) {
+          currentValue = null;
+        } else {
+          currentValue = value;
+        }
+        widget.onPressed(currentValue);
+      });
+    };
+    if (value == currentValue) {
+      return FilledButton(onPressed: onPressed, child: Text(value));
+    }
+    return ElevatedButton(onPressed: onPressed, child: Text(value));
   }
 }
