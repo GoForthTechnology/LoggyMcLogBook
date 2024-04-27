292763f1531f95e2046b0d7473ebfc13b84b9df6
Parker Roth <parkeroth@gmail.com>
2024-04-27 12:26:16 -0500

    Child CRUD via FireStore

--- lib/models/child_model.dart
+++ lib/models/child_model.dart
@@ -1,21 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:lmlb/entities/child.dart';
+import 'package:lmlb/persistence/firebase/firestore_subcollection_crud.dart';
 
 class ChildModel extends ChangeNotifier {
-  final Map<String, List<Child>> childIndex = {};
+  final _persistence = FireStoreSubcollectionCrud<Child>(
+    primaryCollectionName: "clients",
+    subcollectionName: "children",
+    fromJson: (snapshot) =>
+        Child.fromJson(snapshot.data() ?? {}).setId(snapshot.id),
+    toJson: (child) => child.toJson(),
+  );
 
   Future<void> addChild(String clientID, Child child) async {
-    childIndex.putIfAbsent(clientID, () => []);
-    childIndex[clientID]!.add(child);
+    await _persistence.addDoc(doc: child, primaryDocID: clientID);
     notifyListeners();
   }
 
   Future<void> removeChild(String clientID, Child child) async {
-    childIndex[clientID]!.remove(child);
-    notifyListeners();
+    await _persistence.removeDoc(primaryDocID: clientID, docID: child.id!);
   }
 
   Stream<List<Child>> children(String clientID) async* {
-    yield childIndex[clientID] ?? [];
+    yield* _persistence
+        .streamCollection(primaryDocID: clientID)
+        .map((children) {
+      List<Child> out = [];
+      for (var child in children) {
+        if (child != null) {
+          out.add(child);
+        }
+      }
+      return out;
+    });
   }
 }
---
+++ lib/persistence/firebase/firestore_subcollection_crud.dart
@@ -0,0 +1,72 @@
+import 'dart:async';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class FireStoreSubcollectionCrud<T> {
+  final FirebaseFirestore _db;
+  final _userCompleter = new Completer<User>();
+
+  final String primaryCollectionName;
+  final String subcollectionName;
+  final T Function(DocumentSnapshot<Map<String, dynamic>>) fromJson;
+  final Map<String, dynamic> Function(T) toJson;
+
+  FireStoreSubcollectionCrud(
+      {required this.primaryCollectionName,
+      required this.subcollectionName,
+      required this.fromJson,
+      required this.toJson})
+      : _db = FirebaseFirestore.instance {
+    FirebaseAuth.instance.authStateChanges().listen((user) async {
+      if (user != null && !_userCompleter.isCompleted) {
+        _userCompleter.complete(user);
+      }
+    });
+  }
+
+  Future<CollectionReference<T>> _ref(String primaryDocID) async {
+    var user = await _userCompleter.future.timeout(Duration(seconds: 10));
+    return _db
+        .collection("users")
+        .doc(user.uid)
+        .collection(primaryCollectionName)
+        .doc(primaryDocID)
+        .collection(subcollectionName)
+        .withConverter<T>(
+          fromFirestore: (snapshots, _) => fromJson(snapshots),
+          toFirestore: (value, _) => toJson(value),
+        );
+  }
+
+  Stream<T?> streamDoc(
+      {required String docID, required String primaryDocID}) async* {
+    var query = await _ref(primaryDocID);
+
+    yield* query.doc(docID).snapshots().map((s) => s.data());
+  }
+
+  Stream<List<T?>> streamCollection({required String primaryDocID}) async* {
+    var query = await _ref(primaryDocID);
+    yield* query.snapshots().map((s) => s.docs.map((qs) => qs.data()).toList());
+  }
+
+  Future<void> addDoc({required T doc, required String primaryDocID}) async {
+    var query = await _ref(primaryDocID);
+    query.add(doc);
+  }
+
+  Future<void> removeDoc(
+      {required String primaryDocID, required String docID}) async {
+    var query = await _ref(primaryDocID);
+    query.doc(docID).delete();
+  }
+
+  Future<void> update(
+      {required T doc,
+      required String primaryDocID,
+      required String docID}) async {
+    var query = await _ref(primaryDocID);
+    query.doc(docID).update(toJson(doc));
+  }
+}
